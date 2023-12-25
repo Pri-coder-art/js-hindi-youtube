952a004a67f645a9f9abee62e873a7ba33159bb5
priti roy <68179013+Pri-coder-art@users.noreply.github.com>
2023-12-25 04:34:33 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId= 1234
+let accountEmail="Prityroy540@gmail.com"
+var accountPassword= "235"
+accountCity="patna"
+let accountState;
+
+//accountId=23// not allowed
+accountEmail="rityroy540@gmail.com"
+accountPassword= "231"
+accountCity="ahemdavad"
+console.table([accountCity,accountEmail,accountPassword,accountState])
+
+console.log(accountId)
+/*
+prefer not to use var
+because of issue in block scope and functional scope.
+*/
