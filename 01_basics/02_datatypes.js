b33cf11172b92f0478b7e4365d7a5c5b2d94c1b2
priti roy <68179013+Pri-coder-art@users.noreply.github.com>
2023-12-25 04:34:59 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict";// treat all js code a new version.
+
+//alert(3 + 3)  // we are using nodejs, not browser.
+//console.log(3+3); console("hello priti")   //this is not a readable code.
+console.log("hitesh")
+let name="priti"
+let age=23
+let isLoggedIn= false
+
+
+//number= 2 to power 53 
+//bigint 
+//string= " "
+//boolean= true/false
+//null=stanalone value / type of null is object.
+//undefined= means value is not assinged
+//symbol= unique
+console.log(typeof "hitesh")
